3c59c4a9a86af8659916b70d7b88438186e38274
cyt <tim376755000@gmail.com>
2020-04-14 02:35:35 +0800

    make timeline more pretty

---
+++ js/time-line.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#showTimeLine').click(function() {
+        $('#timelineDetail').removeClass('hide');
+    });
+    $('#closeDetailTimeline').click(function() {
+        $('#timelineDetail').addClass('hide');
+    });
+
+
+
+});
