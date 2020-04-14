39a35344744a697ad9d62e0a50cdbda0eec83482
cyt <tim376755000@gmail.com>
2020-04-15 00:30:13 +0800

    add good animate of timeline

--- js/time-line.js
+++ js/time-line.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
+    $('#timelineDetail').hide();
     $('#showTimeLine').click(function() {
-        $('#timelineDetail').removeClass('hide');
+        if ($(timelineDetail).is(":visible")) {
+            $('#timelineDetail').hide('slow');
+        } else {
+            $('#timelineDetail').show('slow');
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $("#timelineDetail").offset().top
+            }, 1000);
+        }
     });
     $('#closeDetailTimeline').click(function() {
-        $('#timelineDetail').addClass('hide');
+        $('#timelineDetail').hide('slow');
     });
-
-
 
 });
