5c9117a440033c324e34fb4e3e73887e721ca9b3
Tim Chen <tim376755000@gmail.com>
2023-04-24 14:46:24 +0800

    try

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three/build/three.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
